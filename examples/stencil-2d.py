08c19a3c52c712e3f766abb3aca5970f0f0c5c76
Frank Schlimbach <frank.schlimbach@intel.com>
2024-04-17 12:31:40 +0200

    gather after rename

--- examples/stencil-2d.py
+++ examples/stencil-2d.py
@@ -197,8 +197,8 @@ def main():
     # * Analyze and output results.
     # ******************************************************************************
 
-    B = np.spmd.gather(B)
-    norm = np.linalg.norm(np.reshape(B, n * n), ord=1)
+    B = np.spmd.gather(np.reshape(B, (n * n,)))
+    norm = np.linalg.norm(B, ord=1)
     active_points = (n - 2 * r) ** 2
     norm /= active_points
 
